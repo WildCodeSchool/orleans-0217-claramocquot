c961f517f7dc8f8816760e652d03d5a21b71f305
Delgesu2 <xvr.cello@gmail.com>
2017-03-30 13:23:19 +0200

    premier commit de la page. Ameliorer la présentation de la page cible des liens.

--- articles.php
+++ articles.php
@@ -5,75 +5,176 @@
 
 <body>
     <div class="container-fluid">
+
         <div class="row">
             <div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1">
 
                     <div class="row">
+                        <div class="col-md-12 col-xs-12">
+                            <div class="jumbotron">
+                                <h1>Zone de texte d'accueil.</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
                         <div class="col-md-4 col-xs-12">
-                            <div class="thumbnail">
+                            <a href="art.html" target=_blank"><div class="thumbnail">
                                 <img src="image/article1.3.jpg" alt="...">
                                 <div class="caption">
-                                    <h3>Thumbnail label</h3>
-                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                    <div class="row">
+
+                                        <div class="col-md-4 col-xs-4 petit">
+                                            <p>
+                                                <h3>Titre</h3>
+                                            </p>
+                                            <p>
+                                                <h4>Date</h4>
+                                            </p>
+                                        </div>
+
+                                        <div class="col-md-8 col-xs-8 grand">
+                                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+                                        </div>
+
+                                    </div>
                                 </div>
-                            </div>
+                                </div></a>
+                        </div>
+
+
+                        <div class="col-md-4 col-xs-12">
+                            <a href="art.html" target=_blank><div class="thumbnail">
+                                <img src="image/article1.3.jpg" alt="...">
+                                <div class="caption">
+                                    <div class="row">
+
+                                        <div class="col-md-4 col-xs-4 petit">
+                                            <p>
+                                            <h3>Titre</h3>
+                                            </p>
+                                            <p>
+                                            <h4>Date</h4>
+                                            </p>
+                                        </div>
+
+                                        <div class="col-md-8 col-xs-8 grand">
+                                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                </div></a>
                         </div>
 
                         <div class="col-md-4 col-xs-12">
-                            <div class="thumbnail">
+                            <a href="art.html" target=_blank><div class="thumbnail">
                                 <img src="image/article1.3.jpg" alt="...">
                                 <div class="caption">
-                                    <h3>Thumbnail label</h3>
-                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                                </div>
-                            </div>
-                        </div>
+                                    <div class="row">
 
-                        <div class="col-md-4 col-xs-12">
-                            <div class="thumbnail">
-                                <img src="image/article1.3.jpg" alt="...">
-                                <div class="caption">
-                                    <h3>Thumbnail label</h3>
-                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                        <div class="col-md-4 col-xs-4 petit">
+                                            <p>
+                                            <h3>Titre</h3>
+                                            </p>
+                                            <p>
+                                            <h4>Date</h4>
+                                            </p>
+                                        </div>
+
+                                        <div class="col-md-8 col-xs-8 grand">
+                                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+                                        </div>
+
+                                    </div>
                                 </div>
-                            </div>
+                                </div></a>
                         </div>
                     </div>
 
 
                     <div class="row">
                         <div class="col-md-4 col-xs-12">
-                            <div class="thumbnail">
+                            <a href="art.html" target=_blank><div class="thumbnail">
                                 <img src="image/article1.3.jpg" alt="...">
                                 <div class="caption">
-                                    <h3>Thumbnail label</h3>
-                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                    <div class="row">
+
+                                        <div class="col-md-4 col-xs-4 petit">
+                                            <p>
+                                            <h3>Titre</h3>
+                                            </p>
+                                            <p>
+                                            <h4>Date</h4>
+                                            </p>
+                                        </div>
+
+                                        <div class="col-md-8 col-xs-8 grand">
+                                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+                                        </div>
+
+                                    </div>
                                 </div>
-                            </div>
+                                </div></a>
                         </div>
 
                         <div class="col-md-4 col-xs-12">
-                            <div class="thumbnail">
+                            <a href="art.html" target=_blank><div class="thumbnail">
                                 <img src="image/article1.3.jpg" alt="...">
                                 <div class="caption">
-                                    <h3>Thumbnail label</h3>
-                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                    <div class="row">
+
+                                        <div class="col-md-4 col-xs-4 petit">
+                                            <p>
+                                            <h3>Titre</h3>
+                                            </p>
+                                            <p>
+                                            <h4>Date</h4>
+                                            </p>
+                                        </div>
+
+                                        <div class="col-md-8 col-xs-8 grand">
+                                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+                                        </div>
+
+                                    </div>
                                 </div>
-                            </div>
+                                </div></a>
                         </div>
 
                         <div class="col-md-4 col-xs-12">
-                            <div class="thumbnail">
+                            <a href="art.html" target=_blank><div class="thumbnail">
                                 <img src="image/article1.3.jpg" alt="...">
                                 <div class="caption">
-                                    <h3>Thumbnail label</h3>
-                                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                                    <div class="row">
+
+                                        <div class="col-md-4 col-xs-4 petit">
+                                            <p>
+                                            <h3>Titre</h3>
+                                            </p>
+                                            <p>
+                                            <h4>Date</h4>
+                                            </p>
+                                        </div>
+
+                                        <div class="col-md-8 col-xs-8 grand">
+                                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+                                        </div>
+
+                                    </div>
                                 </div>
-                            </div>
+                                </div></a>
                         </div>
+                    </div>
             </div>
         </div>
     </div>
+
 </body>
 
 
