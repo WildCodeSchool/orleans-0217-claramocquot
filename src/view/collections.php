983f2a913ef13dfdbc4e243a4d284790804695f2
Delgesu2 <xvr.cello@gmail.com>
2017-04-05 11:33:05 +0200

    infobulles

--- src/view/collections.php
+++ src/view/collections.php
@@ -16,86 +16,104 @@ require 'header.php';
 
                     <div class="row">
                         <div class="col-xs-12 col-md-4">
-                            <div class="thumbnailHome">
-                                <img class="thumbnail"
-                                     src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
-                                     alt="thumbnail image">
-                                <h1 class="titre_photo">Chapeau d'hiver</h1>
-                            </div>
+                            <a href="produit.php">
+                                <div class="thumbnailHome">
+                                    <img class="thumbnail"
+                                         src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
+                                         alt="thumbnail image">
+                                    <h1 class="titre_photo">Chapeau d'hiver</h1>
+                                </div>
+                            </a>
                         </div>
 
                         <div class="col-xs-12 col-md-4">
-                            <div class="thumbnailHome">
-                                <img class="thumbnail"
-                                     src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
-                                     alt="thumbnail image">
-                                <h1 class="titre_photo">Chapeau d'été</h1>
-                            </div>
+                            <a href="produit.php">
+                                <div class="thumbnailHome">
+                                    <img class="thumbnail"
+                                         src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
+                                         alt="thumbnail image">
+                                    <h1 class="titre_photo">Chapeau d'été</h1>
+                                </div>
+                            </a>
                         </div>
 
                         <div class="col-xs-12 col-md-4 gris">
-                            <div class="thumbnailHome">
-                                <img class="thumbnail"
-                                     src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
-                                     alt="thumbnail image">
-                                <h1 class="titre_photo">Chapeau d'lapin</h1>
-                            </div>
+                            <a href=#>
+                                <div class="thumbnailHome">
+                                    <img class="thumbnail"
+                                         src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
+                                         alt="thumbnail image" title="Produit actuellement indisponible">
+                                    <h1 class="titre_photo">Chapeau d'lapin</h1>
+                                </div>
+                            </a>
                         </div>
                     </div>
 
 
                     <div class="row">
                         <div class="col-xs-12 col-md-4">
-                            <div class="thumbnailHome">
-                                <img class="thumbnail"
-                                     src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
-                                     alt="thumbnail image">
-                                <h1 class="titre_photo">Chapeau d'échappement</h1>
-                            </div>
+                            <a href="produit.php">
+                                <div class="thumbnailHome">
+                                    <img class="thumbnail"
+                                         src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
+                                         alt="thumbnail image">
+                                    <h1 class="titre_photo">Chapeau d'échappement</h1>
+                                </div>
+                            </a>
                         </div>
                         <div class="col-xs-12 col-md-4">
-                            <div class="thumbnailHome">
-                                <img class="thumbnail"
-                                     src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
-                                     alt="thumbnail image">
-                                <h1 class="titre_photo">Chapeau de miel</h1>
-                            </div>
+                            <a href="produit.php">
+                                <div class="thumbnailHome">
+                                    <img class="thumbnail"
+                                         src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
+                                         alt="thumbnail image">
+                                    <h1 class="titre_photo">Chapeau de miel</h1>
+                                </div>
+                            </a>
                         </div>
                         <div class="col-xs-12 col-md-4">
-                            <div class="thumbnailHome">
-                                <img class="thumbnail"
-                                     src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
-                                     alt="thumbnail image">
-                                <h1 class="titre_photo">Chapeau chapi</h1>
-                            </div>
+                            <a href="produit.php">
+                                <div class="thumbnailHome">
+                                    <img class="thumbnail"
+                                         src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
+                                         alt="thumbnail image">
+                                    <h1 class="titre_photo">Chapeau chapi</h1>
+                                </div>
+                            </a>
                         </div>
                     </div>
 
 
                     <div class="row">
                         <div class="col-xs-12 col-md-4">
-                            <div class="thumbnailHome">
-                                <img class="thumbnail"
-                                     src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
-                                     alt="thumbnail image">
-                                <h1 class="titre_photo">Chapeau de printemps</h1>
-                            </div>
+                            <a href="produit.php">
+                                <div class="thumbnailHome">
+                                    <img class="thumbnail"
+                                         src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
+                                         alt="thumbnail image">
+                                    <h1 class="titre_photo">Chapeau de printemps</h1>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-xs-12 col-md-4 gris">
+                            <a href=#>
+                                <div class="thumbnailHome">
+                                    <img class="thumbnail"
+                                         src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
+                                         alt="thumbnail image" title="Produit actuellement indisponible">
+                                    <h1 class="titre_photo">Chapeau d'automne</h1>
+                                </div>
+                            </a>
                         </div>
                         <div class="col-xs-12 col-md-4">
-                            <div class="thumbnailHome">
-                                <img class="thumbnail"
-                                     src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
-                                     alt="thumbnail image">
-                                <h1 class="titre_photo">Chapeau d'automne</h1>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-md-4">
-                            <div class="thumbnailHome">
-                                <img class="thumbnail"
-                                     src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
-                                     alt="thumbnail image">
-                                <h1 class="titre_photo">Chapeau de <pluie></pluie></h1>
-                            </div>
+                            <a href="produit.php">
+                                <div class="thumbnailHome">
+                                    <img class="thumbnail"
+                                         src="https://www.chapellerie-traclet.com/37425-large/chapeau-fez-bordeaux.jpg"
+                                         alt="thumbnail image">
+                                    <h1 class="titre_photo">Chapeau Lopopo</pluie></h1>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
