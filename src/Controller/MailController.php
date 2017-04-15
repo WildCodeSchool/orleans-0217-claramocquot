ec243244e13ecfbe8c429449f7c5eb5ec22cf9b6
julien <julienmartin.opto@gmail.com>
2017-04-15 22:10:23 +0200

    refonte du bouton burger et du javascript

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -11,8 +11,17 @@ namespace Clara\Controller;
 
 class MailController extends Controller
 {
-    public function sendMail()
-    {
 
+    public function sendMail($data)
+    {
+        if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) {
+            $lineBreak = "\r\n";
+        } else {
+            $lineBreak = "\n";
+        }
+        $header = "From: \"WeaponsB\"<weaponsb@mail.fr>" . $lineBreak;
+        $header .= "Reply-to: \"WeaponsB\" <weaponsb@mail.fr>" . $lineBreak;
+        $header .= "MIME-Version: 1.0" . $lineBreak;
+        $header .= "Content-Type: multipart/alternative;" . $lineBreak . " boundary=\"$boundary\"" . $lineBreak;
     }
 }
