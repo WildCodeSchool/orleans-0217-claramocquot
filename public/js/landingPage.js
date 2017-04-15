28c7066929a41d1ab89a10da7786d55bd1771c44
julien <julienmartin.opto@gmail.com>
2017-04-16 01:57:50 +0200

    envoi de mail fonctionnel

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -40,6 +40,16 @@ $(function() {
         $(this).find('.dropdown-menu').first().stop(true, true).fadeToggle(300);
     });
 
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.8";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
 });
 
 
