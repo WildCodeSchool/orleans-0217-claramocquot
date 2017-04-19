86d433e286d4138e0d627ba1ca0864041cd5b910
julien <julienmartin.opto@gmail.com>
2017-04-19 19:14:52 +0200

    slider page home et thumbnail responsive

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -18,7 +18,7 @@ $(function() {
 
     function link() {
         var scroll = $(window).scrollTop();
-            if (scroll <= 1000) {
+            if (scroll <= 950) {
                 $('#link').addClass('hidden');
             } else {
                 $('#link').removeClass('hidden');
