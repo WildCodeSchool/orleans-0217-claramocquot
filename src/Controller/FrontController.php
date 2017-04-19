f935882bf1fa9aade2b718108b48c192c38fe44e
tderot <derot7@gmail.com>
2017-04-19 20:51:53 +0200

    mise en place des formulaire envoie de mail plus diffenrents choses

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -8,11 +8,18 @@
 
 namespace Clara\Controller;
 
-
+use Clara\Controller\MailController;
 use Clara\Model\ContentManager;
 use Clara\Model\HatManager;
 use Clara\Model\PictureHomeManager;
 use Clara\Model\Visibility_marraineManager;
+
+require __DIR__ . '/../../vendor/swiftmailer/swiftmailer/lib/swift_required.php';
+use Swift_SmtpTransport;
+use Swift_Mailer;
+use Swift_Message;
+use WindowsAzure\ServiceManagement\Models\Location;
+
 
 class FrontController extends Controller
 {
@@ -26,7 +33,13 @@ class FrontController extends Controller
         $lastPortrait = $em->findLastType('portrait');
         $newHats = $em2->showNewHats();
         $picture = $em3->findPictureHome();
-        return $this->getTwig()->render('home.html.twig', ['evenement' => $lastEvenement, 'blog' => $lastBlog, 'portrait' => $lastPortrait, 'news'=>$newHats, 'picture'=>$picture]);
+        $msg = new MailController(true);
+        if (isset($_POST['submitcontact'])) {
+            $msg->contact($_POST);
+            header('Location:index.php');
+        }
+        return $this->getTwig()->render('home.html.twig', ['evenement' => $lastEvenement, 'blog' => $lastBlog, 'portrait' => $lastPortrait, 'news' => $newHats, 'picture' => $picture]);
+
     }
 
     /**
@@ -42,25 +55,66 @@ class FrontController extends Controller
 
     public function showContents($type)
     {
+        $msg = new MailController(true);
         $db = new ContentManager();
         $em = new Visibility_marraineManager();
         $visibility = $em->showVisibility();
         $res = $db->findAll($type);
-        return $this->getTwig()->render('contents.html.twig', ['datas'=>$res,'type'=>$type, 'visibility'=>$visibility]);
+
+        if (isset($_POST['submitcontact'])) {
+            $msg->contact($_POST);
+            header('Location:index.php?route=articles&type=' . $type);
+
+        }
+        if (isset($_POST['submitmarraine'])) {
+            $msg->contactMarraine($_POST);
+            header('Location:index.php?route=articles&type=' . $type);
+        }
+
+        if (isset($_POST['submittemoignage'])) {
+            $msg->contactTemoignage($_POST);
+            header('Location:index.php?route=articles&type=' . $type);
+        }
+
+        if (isset($_POST['submitpartenaire'])) {
+            $msg->contactPartenaire($_POST);
+            header('Location:index.php?route=articles&type=' . $type);
+        }
+
+        return $this->getTwig()->render('contents.html.twig', ['datas' => $res, 'type' => $type, 'visibility' => $visibility]);
     }
 
     public function showHats($res)
     {
-
+        $msg = new MailController(true);
         $em = new HatManager();
         $datas = $em->showHats();
-        $oldHats= $em->showOldHats();
-        return $this->getTwig()->render('products.html.twig', ['datas' => $datas, 'res' => $res, 'olds' =>$oldHats]);
+        $oldHats = $em->showOldHats();
+
+        if (isset($_POST['submitproduit'])) {
+            $msg->contactProduct($_POST);
+            header('Location:index.php?route=produits');
+
+        }
+
+        return $this->getTwig()->render('products.html.twig', ['datas' => $datas, 'res' => $res, 'olds' => $oldHats]);
     }
+
     public function showHat($id)
     {
+        $msg = new MailController(true);
         $db = new HatManager();
         $res = $db->showHat($id);
+        if (isset($_POST['submitproduit'])) {
+            $msg->contactProduct($_POST);
+            header('Location:index.php?route=produit&id=' . $id);
+
+        }
+        if (isset($_POST['submitcontact'])) {
+            $msg->contact($_POST);
+            header('Location:index.php?route=produit&id=' . $id);
+
+        }
         return $this->getTwig()->render('product.html.twig', ['hats' => $res]);
     }
 
--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -8,30 +8,137 @@
 
 namespace Clara\Controller;
 require __DIR__ . '/../../vendor/swiftmailer/swiftmailer/lib/swift_required.php';
-
+use Swift_SmtpTransport;
+use Swift_Mailer;
+use Swift_Message;
 
 class MailController extends Controller
 {
-    public function product()
+
+
+
+    public function contactProduct($data)
     {
-        if (isset($_POST['submit'])) {
-            $transport = Swift_SmtpTransport::newInstance('smtp.googlemail.com', 465, 'ssl')
-                ->setUsername('lefadak3@gmail.com')
-                ->setPassword('ahglagla');
-            $mailer = Swift_Mailer::newInstance($transport);
-            $message = Swift_Message::newInstance('Renseignements Commande')
-                ->setSubject('Renseignements Commande')
-                ->setFrom(array($_POST['email'] => $_POST['firstname'] . ' ' . $_POST['name']))
-                ->setTo(array('derot7@gmail.com' => 'Julien Martin'))
-                ->setDate(time())
-                ->addPart($_POST['address'] . '<br/>' . $_POST['size'] . '<br/>' . $_POST['comment'] . '<br/>');
-            $res = $mailer->send($message);
-            echo $res;
+        $body = '<h1><b>Demande renseignement Produit :</b></h1><br>';
+        foreach ($data as $key => $value) {
+            if ($key != 'submitproduit') {
+                $body .= '<b>' . $key . '</b> : ' . $value . '<br>';
+            }
         }
-        return $this->getTwig()->render('product.html.twig');
+
+        $transport = Swift_SmtpTransport::newInstance('smtp.googlemail.com', 465, 'ssl')
+            ->setUsername('lefadak3@gmail.com')
+            ->setPassword('ahglagla');
+        $mailer = Swift_Mailer::newInstance($transport);
+        $message = Swift_Message::newInstance('Renseignements Commande')
+            ->setSubject('Renseignements Commande')
+            ->setFrom(array($_POST['Email'] => $_POST['Prenom'] . ' ' . $_POST['Nom']))
+            ->setReplyTo($_POST['Email'])
+            ->setTo(array('derot7@gmail.com' => 'Julien Martin'))
+            ->setDate(time())
+            ->setBody($body, 'text/html');
+        $mailer->send($message);
+
     }
 
 
 
+
+    public function contact($data)
+    {
+        $body = '<h1><b>Demande renseignements :</b></h1><br>';
+        foreach ($data as $key => $value) {
+            if ($key != 'submitcontact') {
+                $body .= '<b>' . $key . '</b> : ' . $value . '<br>';
+            }
+        }
+
+        $transport = Swift_SmtpTransport::newInstance('smtp.googlemail.com', 465, 'ssl')
+            ->setUsername('lefadak3@gmail.com')
+            ->setPassword('ahglagla');
+        $mailer = Swift_Mailer::newInstance($transport);
+        $message = Swift_Message::newInstance('Prise de contact')
+            ->setSubject('Prise de contact')
+            ->setFrom(array($_POST['Email'] => $_POST['Prenom'] . ' ' . $_POST['Nom']))
+            ->setReplyTo($_POST['Email'])
+            ->setTo(array('derot7@gmail.com' => 'Julien Martin'))
+            ->setDate(time())
+            ->setBody($body, 'text/html');
+        $mailer->send($message);
+    }
+
+
+
+
+    public function contactMarraine($data)
+    {
+        $body = '<h1><b>Demande Marraine :</b></h1><br>';
+        foreach ($data as $key => $value) {
+            if ($key != 'submitmarraine') {
+                $body .= '<b>' . $key . '</b> : ' . $value . '<br>';
+            }
+        }
+
+        $transport = Swift_SmtpTransport::newInstance('smtp.googlemail.com', 465, 'ssl')
+            ->setUsername('lefadak3@gmail.com')
+            ->setPassword('ahglagla');
+        $mailer = Swift_Mailer::newInstance($transport);
+        $message = Swift_Message::newInstance('Demande Marraine')
+            ->setSubject('Demande Marraine')
+            ->setFrom(array($_POST['Email'] => $_POST['Prenom'] . ' ' . $_POST['Nom']))
+            ->setReplyTo($_POST['Email'])
+            ->setTo(array('derot7@gmail.com' => 'Julien Martin'))
+            ->setDate(time())
+            ->setBody($body, 'text/html');
+        $mailer->send($message);
+    }
+
+
+    public function contactTemoignage($data)
+    {
+        $body = '<h1><b>Proposition de témoignage : </b></h1><br>';
+        foreach ($data as $key => $value) {
+            if ($key != 'submittemoignage') {
+                $body .= '<b>' . $key . '</b> : ' . $value . '<br>';
+            }
+        }
+
+        $transport = Swift_SmtpTransport::newInstance('smtp.googlemail.com', 465, 'ssl')
+            ->setUsername('lefadak3@gmail.com')
+            ->setPassword('ahglagla');
+        $mailer = Swift_Mailer::newInstance($transport);
+        $message = Swift_Message::newInstance('Proposition témoignage')
+            ->setSubject('Proposition témoignage')
+            ->setFrom(array($_POST['Email'] => $_POST['Prenom'] . ' ' . $_POST['Nom']))
+            ->setReplyTo($_POST['Email'])
+            ->setTo(array('derot7@gmail.com' => 'Julien Martin'))
+            ->setDate(time())
+            ->setBody($body, 'text/html');
+        $mailer->send($message);
+    }
+
+
+    public function contactPartenaire($data)
+    {
+        $body = '<h1><b>Proposition partenaire : </b></h1><br>';
+        foreach ($data as $key => $value) {
+            if ($key != 'submitpartenaire') {
+                $body .= '<b>' . $key . '</b> : ' . $value . '<br>';
+            }
+        }
+
+        $transport = Swift_SmtpTransport::newInstance('smtp.googlemail.com', 465, 'ssl')
+            ->setUsername('lefadak3@gmail.com')
+            ->setPassword('ahglagla');
+        $mailer = Swift_Mailer::newInstance($transport);
+        $message = Swift_Message::newInstance('Renseignements Partenaires')
+            ->setSubject('Renseignements Partenaires')
+            ->setFrom(array($_POST['Email'] => $_POST['Prenom'] . ' ' . $_POST['Nom']))
+            ->setReplyTo($_POST['Email'])
+            ->setTo(array('derot7@gmail.com' => 'Julien Martin'))
+            ->setDate(time())
+            ->setBody($body, 'text/html');
+        $mailer->send($message);
+    }
 
 }
