836e5434a18dafa4131ae08e4651efea5c597b7d
tderot <derot7@gmail.com>
2017-05-04 15:32:05 +0200

    finalisation et derniere modif

--- src/Form/Validator/ImageValidators.php
+++ src/Form/Validator/ImageValidators.php
@@ -43,10 +43,10 @@ class ImageValidators implements ValidatorInterface
         if (!empty($image)) {
             if (getimagesize($image)) {
                 $size = getimagesize($image);
-                if ($size[0] == 500 && $size[1] == 500) {
+                if ($size[0] == 700 && $size[1] == 700) {
                     return true;
                 } else {
-                    $this->setNotValid([0 => 'L\'image envoyée n\'est pas au bon format de 500x500!']);
+                    $this->setNotValid([0 => 'L\'image envoyée n\'est pas au bon format de 700x700!']);
 
                 }
             } else {
@@ -64,7 +64,7 @@ class ImageValidators implements ValidatorInterface
                 if ($size[0] == 1920 && $size[1] == 1080) {
                     return true;
                 } else {
-                    $this->setNotValid([0 => 'L\'image envoyée n\'est pas au bon format de 500x500!']);
+                    $this->setNotValid([0 => 'L\'image envoyée n\'est pas au bon format de 700x700!']);
 
                 }
             } else {
